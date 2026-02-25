3fcc7e60a69d18d0caf99d928e64f90d288cba1e
Aida <aida_ying@hotmail.com>
2026-02-25 21:51:17 +1300

    Add project files.

---
+++ ParcelsOrderService.Test/ParcelOrderServiceTest.cs
@@ -0,0 +1,25 @@
+﻿namespace ParcelsOrderService.Test
+{
+    public class ParcelOrderServiceTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            var parcels = new List<Parcel>
+            {
+                new Parcel (2.00,3.00,2.00 ),
+                new Parcel (15.00,55.00,20.00 ),
+                new Parcel (20.00,50.00,55.00 ),
+                new Parcel (100.00,80.00,70.00 )
+            };
+
+            var orders = new Order (parcels);
+
+            foreach (var parcel in orders.Parcels)
+            {
+                Console.WriteLine ($"{parcel.Cost}    {parcel.SizeCategory}");
+            }
+            Console.WriteLine($"Total Cost: {orders.TotalCost}");
+        }
+    }
+}
